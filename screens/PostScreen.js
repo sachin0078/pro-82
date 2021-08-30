b8450463032ecaebf128ef9f01c5367e39333a9c
sachin0078 <68186774+sachin0078@users.noreply.github.com>
2021-08-30 13:16:56 +0530

    Add files via upload

---
+++ screens/PostScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {  View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Image,
+    ScrollView,
+    TextInput,} from 'react-native';
+import * as Font from "expo-font";
+import DropDownPicker from "react-native-dropdown-picker";
+
+export default class CreatePost extends React.Component{
+    render(){
+        return(
+            <View></View>
+        )
+    }
+}
